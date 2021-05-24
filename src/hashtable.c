13bdabc76edcbab2865a79c77b0a3c141b90ed06
Rafael Oliveira <rafdev.x@gmail.com>
2021-05-24 15:10:58 +0100

    comment hashtable.c

--- src/hashtable.c
+++ src/hashtable.c
@@ -7,6 +7,8 @@
 /*** Include project header file ***/
 #include "filesystem.h"
 
+/* Allocate memory for and instantiate a new hash table with the specified
+ * dimension, getKey function and hash function */
 HashT *newHashT(long int dim, void *getKey(void *),
 				long int (*hash)(void *, long int)) {
 	long int i;
@@ -21,23 +23,28 @@ HashT *newHashT(long int dim, void *getKey(void *),
 	return table;
 }
 
+/* Return a DLL of all possible values from a hash table which might match a
+ * given key */
 DLL *searchHashT(HashT *table, void *key) {
 	long int hash = table->hash(key, table->dim);
 	DLL *lst = table->entries[hash];
 	return lst;
 }
 
+/* Insert a new value into a hash table */
 void insertHashT(HashT *table, void *value) {
 	void *key = table->getKey(value);
 	pushDLL(searchHashT(table, key), value);
 }
 
+/* Remove a given value from a hash table */
 void removeHashT(HashT *table, void *value) {
 	void *key = table->getKey(value);
 	DLL *lst = searchHashT(table, key);
 	removeDLL(lst, value, 1, NULL);
 }
 
+/* Delete/free a hash table and everything within (without freeing values) */
 void freeHashT(HashT *table) {
 	long int i;
 	for (i = 0; i < table->dim; i++) {
